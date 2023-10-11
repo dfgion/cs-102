440f338b8f19a7d82f9c1bce324339b61a649d21
Daniil Kutukov <ddddanyaaa@gmail.com>
2023-10-12 01:43:20 +0300

    Update calc

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,21 @@
 import unittest
+import sys
+sys.path.append(r'C:\Users\Даниил\Desktop\cs-102')
+from src.lab1.calculator import summ, difference, multiply, division, exponentiation
 
 class CalculatorTestCase(unittest.TestCase):
+    
+    def test_summ(self):
+        self.assertEquals(summ(1, 2), 3)
 
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+    def test_difference(self):
+        self.assertEquals(difference(4, 2), 2)
+
+    def test_multiply(self):
+        self.assertEquals(multiply(2, 2), 4)
+
+    def test_division(self):
+        self.assertEquals(division(6, 2), 3)
+
+    def test_exponentiation(self):
+        self.assertEquals(exponentiation(3, 3), 27)
