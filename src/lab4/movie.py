4a8390cbc030309faad39103ad33d2e7a395ec7e
Daniil Kutukov <ddddanyaaa@gmail.com>
2023-12-21 17:20:22 +0300

    fix mis

---
+++ src/lab4/movie.py
@@ -0,0 +1,62 @@
+class Cinema:
+
+    """
+    Класс, который реализует подбор фильма
+    """
+
+    def __init__(self, films_file, history_file, user_views):
+        self.films_file = films_file
+        self.history_file = history_file
+        self.films = self.load_films()
+        self.history = self.load_history()
+        self.user_views = list(map(int, user_views.split(',')))
+
+    def load_films(self):
+        films = {}
+        with open(self.films_file, 'r', encoding="utf-8") as file:
+            for line in file:
+                movie_id, movie_name = line.strip().split(',')
+                films[int(movie_id)] = movie_name
+        return films
+
+    def load_history(self):
+        history = []
+        with open(self.history_file, 'r', encoding="utf-8") as file:
+            for line in file:
+                viewed_films = list(map(int, line.strip().split(',')))
+                history.append(viewed_films)
+        return history
+
+    def filter_users(self):
+        filtered_users = []
+        for user in self.history:
+            if set(self.user_views).issubset(set(user)):
+                filtered_users.append(user)
+        return filtered_users
+
+    def exclude_watched_films(self, filtered_users):
+        unwatched_films = []
+        for user in filtered_users:
+            unwatched_films.extend([movie for movie in user if movie not in self.user_views])
+        return unwatched_films
+
+    def recommend_movie(self):
+        filtered_users = self.filter_users()
+        unwatched_films = self.exclude_watched_films(filtered_users)
+
+        movie_views_count = {}
+        for movie in unwatched_films:
+            movie_views_count[movie] = movie_views_count.get(movie, 0) + 1
+
+        recommended_movie_id = max(movie_views_count, key=movie_views_count.get)
+        recommended_movie_name = self.films[recommended_movie_id]
+
+        print("Рекомендуемый фильм:", recommended_movie_name)
+
+
+if __name__ == "__main__":
+    films_file = r'src\lab4\films.txt'
+    history_file = r'src\lab4\history.txt'
+    user_views = '1, 2'
+    recommendation_service = Cinema(films_file, history_file, user_views)
+    recommendation_service.recommend_movie()
---
+++ tests/lab4/test_movie.py
@@ -0,0 +1,35 @@
+import unittest
+from src.lab4.movie import Cinema
+
+
+class TestCinema(unittest.TestCase):
+
+    def setUp(self):
+        self.films_file = r"src\lab4\films.txt"
+        self.history_file = r"src\lab4\history.txt"
+        self.user_views = "1,2,3"
+        self.cinema = Cinema(
+            self.films_file, self.history_file, self.user_views)
+
+    def test_load_films(self):
+        expected_films = {1: 'Мстители: Финал', 2: 'Хатико',
+                          3: 'Дюна', 4: 'Унесенные призраками'}
+        self.assertEqual(self.cinema.load_films(), expected_films)
+
+    def test_load_history(self):
+        expected_history = [[2, 1, 3], [1, 4, 3], [2, 2, 2, 2, 2, 3]]
+        self.assertEqual(self.cinema.load_history(), expected_history)
+
+    def test_filter_users(self):
+        expected_filtered_users = [[2, 1, 3]]
+        self.assertEqual(self.cinema.filter_users(), expected_filtered_users)
+
+    def test_exclude_watched_films(self):
+        filtered_users = self.cinema.filter_users()
+        expected_unwatched_films = []
+        self.assertEqual(self.cinema.exclude_watched_films(
+            filtered_users), expected_unwatched_films)
+
+
+if __name__ == 'main':
+    unittest.main()
